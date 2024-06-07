8a620e3fe39274fb6413eb2153c5d8a1f043ef22
taloreff <taloreff@gmail.com>
2024-06-07 14:47:17 +0300

    added initial api and services

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://taloreff:3sSn2ZD1p19GjruU@staybnb.bzx3y9d.mongodb.net',
+  dbName: process.env.DB_NAME || 'Staybnb'
+}
--- server.js
+++ server.js
@@ -1,34 +1,59 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const env = require("dotenv");
-const cors = require("cors");
+import express, { json } from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import { connect, Schema, model } from "mongoose";
+import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-env.config();
+config();
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 const PORT = process.env.PORT || 3030;
-const MongoDB = process.env.MongoDB;
+// const MongoDB = process.env.MongoDB;
 
-console.log("MongoDB", MongoDB);
-mongoose
-  .connect(MongoDB)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// console.log("MongoDB", MongoDB);
+// connect(MongoDB)
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173'
+    ],
+    credentials: true
+  }
+  app.use(cors(corsOptions));
+}
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST"],
-};
+app.use(cookieParser());
+app.use(json());
 
-app.use(cors(corsOptions));
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { setupSocketAPI } from './services/socket.service.js '
 
-// Routes
+import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
+app.all('*', setupAsyncLocalStorage)
 
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
 
-app.get("/", (req, res) => {
-  res.send("hello!");
+setupSocketAPI(server)
+
+app.get("/**", (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
 });
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
